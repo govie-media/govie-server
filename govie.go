871e3689ddaa8bb7958177bf1c358ac61e8a9ba9
Daniel Gozick <dgozick@gmail.com>
2024-07-05 13:27:24 -0700

    Server: update structure. setup web server routes, handlers & assets

---
+++ govie.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"govie.io/govie-server/api"
+	web "govie.io/govie-server/client"
+	"govie.io/govie-server/image"
+)
+
+type Govie struct {
+	Version string
+
+	// Servers
+	WebServer   *web.Server
+	ApiServer   *api.Server
+	ImageServer *image.Server
+
+	Tasks string
+}
+
+func (g *Govie) Init(disableWebServer, disableApiServer, disableImageServer bool) {
+	g.Version = "0.0.1"
+
+	// Start Web Server
+	if !disableWebServer {
+		go func() {
+			g.WebServer = &web.Server{}
+			g.WebServer.Init()
+		}()
+	}
+
+	// Start Api Server
+	if !disableApiServer {
+		go func() {
+			g.ApiServer = &api.Server{}
+			g.ApiServer.Init()
+		}()
+	}
+
+	// Start Image Server
+	if !disableImageServer {
+		go func() {
+			g.ImageServer = &image.Server{}
+			g.ImageServer.Init()
+		}()
+	}
+
+	// Wait
+	select {}
+}
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import "flag"
+
 func main() {
-	
+	// Command Arguments
+	disableWebServer := flag.Bool("disableWebServer", false, "(Optional, Boolean) Flag to disable the govie client-web server")
+	disableApiServer := flag.Bool("disableApiServer", false, "(Optional, Boolean) Flag to disable the govie api server")
+	disableImageServer := flag.Bool("disableImageServer", false, "(Optional, Boolean) Flag to disable the govie image server")
+	flag.Parse()
+
+	// Start Govie Servers
+	govie := &Govie{}
+
+	govie.Init(
+		*disableWebServer,
+		*disableApiServer,
+		*disableImageServer,
+	)
 }
